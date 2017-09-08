bd811f72b5bdd3adb7847d751be9727fc8d9f348
Rollin Francois <Rollin@rollins-macbook-pro.local>
2017-09-08 19:51:52 -0400

    Add a UIImageView instance to the default scene and connect it to an outlet in the view controller class.

--- ImagePicker/ViewController.swift
+++ ImagePicker/ViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: Outlets
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
